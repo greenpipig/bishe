4674cd40b2b820ee7ac0391b0110d9cc6ac0e3dc
greenpipig <1105263198@qq.com>
2019-12-07 21:19:17 +0800

    some tool

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import tool
 
 tool.surface()
 choose=input("Please input your choose:")#todo 校验输入
-print(choose)
-if choose == 1:
+#print(choose)
+if choose == "1":
     print("Now you in udpflood method")
-    ip=input("Please input the ip you wanna attack")#todo 校验ip
-    port=input()
-    usertime=input()
-    udpflood.udpfloods(ip,port,usertime)
-elif choose == 2:
+    ip=input("Please input the ip you wanna attack：")#todo 校验ip
+    port=input("Please input the port you wanna attack：")
+    usertime=input("Please input the time you wanna attack：")
+    tool.ping(ip)
+    tool.testport(ip,port)
+    udpflood.udpfloods(ip,port,usertime)#todo string转
+elif choose == "2":
     #todo ddos
     input()
-elif choose == 3:
+elif choose == "3":
     #todo cc
     input()
-elif choose == 4:
+elif choose == "4":
     #todo DBattack
     input()
 else:
--- tool.py
+++ tool.py
@@ -1,3 +1,10 @@
+import os
+import time
+
+PING_RESULT = 0
+NETWORK_RESULT = 0
+
+
 def surface():
     print("=====================================================")
     print("                  恶意流量生成器                       ")
@@ -7,7 +14,46 @@ def surface():
     print("                press 4 in DBattack")
     print("=====================================================")
 
-def getipport():
+
+def getipports():
     ip = input("Please input the ip you wanna attack")  # todo 校验ip
-    port = input("Please input the ip you wanna attack") #todo 校验port
-    return ip,port
+    port = input("Please input the ip you wanna attack")  # todo 校验port
+    return ip, port
+
+
+def disableNetwork():
+    ''' disable network card '''
+    result = os.system(u"netsh interface set interface 以太网 disable")
+    if result == 1:
+        print("disable network card failed")
+    else:
+        print("disable network card successfully")
+
+
+def ping(ip):
+    ''' ping 主备网络 '''
+    result = os.system(u"ping "+ip)
+    # result = os.system(u"ping www.baidu.com -n 3")
+    if result == 0:
+        print("A网正常")
+    else:
+        print("网络故障")
+    return result
+
+
+def testping(ip):
+    PING_RESULT = ping(ip)
+    if PING_RESULT == 0:
+        time.sleep(20)
+    else:
+        disableNetwork()
+        time.sleep(10)
+
+
+def testport(ip, port):
+    #todo 解析码
+    result = os.system(u"curl " + ip+":" + port)
+    if result== 1:
+        print("yes")
+    else:
+        print("no")
