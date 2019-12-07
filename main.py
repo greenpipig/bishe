5152748e5f31f58349f624db82ca0e3730b23ebc
greenpipig <1105263198@qq.com>
2019-12-07 19:32:53 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import udpflood
+
+print("=====================================================")
+print("                  恶意流量生成器                       ")
+print("               press 1 in udpflood                     ")
+print("                 press 2 in ddos                      ")
+print("                  press 3 in cc")
+print("                press 4 in DBattack")
+print("=====================================================")
+choose=input("Please input your choose:")#todo 校验输入
+print(choose)
